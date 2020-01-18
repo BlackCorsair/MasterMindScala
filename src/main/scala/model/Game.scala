28151ed18ab168550f560f4614b815471f1e96b4
Jorge Hevia <jmhev@outlook.com>
2020-01-18 22:19:14 +0100

    Refactor Game.run

--- src/main/scala/model/Game.scala
+++ src/main/scala/model/Game.scala
@@ -1,27 +1,16 @@
 package model
 
 class Game(val board: Board = new Board(), turns: Int = 10) {
-  val _turns = turns
-
   val win = this.board.solved()
 
-  def nextTurn(guess: Guess): Game = this._turns match {
+  def nextTurn(guess: Guess): Game = this.turns match {
     case 0 => this
     case _ => new Game(this.board + guess, this.turns - 1)
   }
 
-  def run(game: Game):Unit = {
-    if (this.win == true) {
-      print("You win!")
-    }
-
-    if (this.win == false && this._turns == 0) {
-      print("You lose!!!")
-    }
-
-    if (this.win == false && this._turns != 0) {
-      run(this.nextTurn(new Guess()))
-    }
-
+  def run(game: Game):String = (this.win, this.turns) match {
+    case (true,_) => "You win!"
+    case (false, 0) => "You lose!!!"
+    case (false, _) => run(this.nextTurn(new Guess()))
   }
 }
