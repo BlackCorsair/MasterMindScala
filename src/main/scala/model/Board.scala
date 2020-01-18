b055a2dcfc245c1797466f2bee9629ce7258814d
Jorge Hevia <jmhev@outlook.com>
2020-01-18 20:53:17 +0100

    Refactor Board and add custom toString

--- src/main/scala/model/Board.scala
+++ src/main/scala/model/Board.scala
@@ -1,17 +1,31 @@
 package model
 
-class Board(solution: Guess = new Guess(), guesses: List[Guess] = Nil) {
+class Board(solution: Guess = new Guess(color.randomList(4)), val guesses: List[Guess] = Nil) {
   def Board(solution: Guess) = new Board(solution, Nil)
 
-  def Board() = new Board(new Guess(Set(color.RED, color.GREEN, color.BLUE, color.BROWN)))
+  def Board() = new Board(new Guess(max=4))
 
   def +(guess: Guess): Board = new Board(this.solution, guess::this.guesses)
 
   def solved(): Boolean = {
     def checkGuess(guesses: List[Guess]): Boolean = guesses match {
-      case head::_ => head == this.solution
+      case Nil => false
+      case head::Nil => head == this.solution
       case head::tail => head == this.solution && checkGuess(tail)
     }
     checkGuess(this.guesses)
   }
+
+  def toString(solution: Boolean = false): String = {
+    def guessesToString(guesses: List[Guess]): String = guesses match {
+      case Nil => ""
+      case head::Nil => return head.toString() + "\n"
+      case head::tail => head.toString() + "\n" + guessesToString(tail)
+    }
+    return "Guesses: \n" + guessesToString(this.guesses) + (if (solution) "Solution:\n" + this.solution.toString() else "")
+  }
+
+  override def toString: String = this.toString(false)
+
 }
+
