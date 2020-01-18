149b9d0907f7f76c6dcdf62ca0d01703547a50cd
Jorge Hevia <jmhev@outlook.com>
2020-01-18 20:55:43 +0100

    Refactor Guess and add toString

--- src/main/scala/model/Guess.scala
+++ src/main/scala/model/Guess.scala
@@ -2,19 +2,45 @@ package model
 
 import model.color.color
 
-class Guess(val colors: Set[color] = Set(), max: Int = 4) {
+class Guess(val colors: List[color] = Nil, max: Int = 4) {
 
   val MAX = max
 
-  def Guess(colors: Set[color]): Guess = new Guess(colors, 4)
+  def Guess(max: Int): Guess = new Guess(Nil, max)
 
-  def addColor(color: color): Guess = this.colors.size match {
+  def Guess(): Guess = new Guess(Nil, 4)
+
+  def +(color: color): Guess = this.colors.size match {
     case this.MAX => this
-    case _ => new Guess(this.colors + color)
+    case _ => new Guess(color::this.colors)
   }
 
-  def ==(other: Guess): Boolean = other match {
-    case other: Guess => (this.colors & other.colors).size == 4
-    case _ => false
+  def ==(other: Guess): Boolean = {
+    def compareLists(l1: List[color], l2: List[color]): Boolean = {
+      if (l1 == Nil && l2 == Nil) return true
+      if (l1.length != l2.length) return false
+      if (l1.head != l2.head) return false
+      compareLists(l1.tail, l2.tail)
+    }
+    other match {
+      case other: Guess => compareLists(this.colors, other.colors)
+      case _ => false
+    }
   }
+
+  def colorToString(c: color): String = c match {
+    case color.RED => "RED"
+    case color.BLUE => "BLUE"
+    case color.BROWN => "BROWN"
+    case color.ORANGE => "ORANGE"
+    case color.GREEN => "GREEN"
+    case color.YELLOW => "YELLOW"
+  }
+  def colorsToString(colors: List[color]): String = colors match{
+    case Nil => ""
+    case head::Nil => colorToString(head)
+    case head::tail => colorToString(head) + " " + colorsToString(tail)
+  }
+
+  override def toString(): String = "[ " + colorsToString(this.colors) + " ]"
 }
