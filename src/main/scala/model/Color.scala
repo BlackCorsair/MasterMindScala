b7ff880b07317a217856f77b7d4846cb21fb0cb3
Jorge Hevia <jmhev@outlook.com>
2020-01-18 20:54:20 +0100

    Add randomList to Random

--- src/main/scala/model/Color.scala
+++ src/main/scala/model/Color.scala
@@ -1,8 +1,19 @@
 package model
 
-import model.color.color
+import scala.util.Random
 
 object color extends Enumeration {
   type color = Value
   val RED, BLUE, BROWN, ORANGE, GREEN, YELLOW = Value
+
+  val all = List(color.RED, color.BLUE, color.BROWN, color.ORANGE, color.GREEN, color.YELLOW)
+
+  def randomList(num: Int): List[color] = {
+    def generateList(num: Int, list: List[color]): List[color] = num match {
+      case 0 => Nil
+      case _ => all(new Random().nextInt(all.length))::generateList(num - 1, list)
+    }
+    if (num >= all.length) return all
+    else return generateList(num, Nil)
+  }
 }
