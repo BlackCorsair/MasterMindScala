e4135d99db96869f5ded27290bc87423625add84
Jorge Hevia <jmhev@outlook.com>
2020-01-18 20:54:52 +0100

    Add initial Game class

--- src/main/scala/model/Game.scala
+++ src/main/scala/model/Game.scala
@@ -9,4 +9,19 @@ class Game(val board: Board = new Board(), turns: Int = 10) {
     case 0 => this
     case _ => new Game(this.board + guess, this.turns - 1)
   }
+
+  def run(game: Game):Unit = {
+    if (this.win == true) {
+      print("You win!")
+    }
+
+    if (this.win == false && this._turns == 0) {
+      print("You lose!!!")
+    }
+
+    if (this.win == false && this._turns != 0) {
+      run(this.nextTurn(new Guess()))
+    }
+
+  }
 }
